837377f1a006e65db6ad5cbb4c3e98ac3a59a21e
sidhu2k15 <63468867+sidhu2k15@users.noreply.github.com>
2020-04-10 23:39:36 +0530

    Changed no of iterations

--- TestJenkins.java
+++ TestJenkins.java
@@ -2,7 +2,7 @@ package org.innovitipos.merchant.report;
 
 public class TestJenkins {
 public static void main(String[] args) {
-	for(int i =0;i< 100;i++) {
+	for(int i =0;i< 50;i++) {
 		System.out.println("Iteration...."+i);
 	}
 }
