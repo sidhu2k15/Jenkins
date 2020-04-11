f2719574dd34a2a890c15bff31dddf6c44f3a09c
Siddharth <siddharth.shinde@innoviti.com>
2020-04-11 13:38:06 +0530

    Commiting changes to delete TestJenkins.java

--- TestJenkins.java
+++
@@ -1,9 +0,0 @@
-package org.innovitipos.merchant.report;
-
-public class TestJenkins {
-public static void main(String[] args) {
-	for(int i =0;i< 50;i++) {
-		System.out.println("Iteration...."+i);
-	}
-}
-}
